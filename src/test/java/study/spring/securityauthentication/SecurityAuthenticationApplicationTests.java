0013adbdd8c8eb36b56d53e64a1707534d0a7e29
hclee <heechul4296@gmail.com>
2022-10-10 14:50:06 +0900

    spring security authentication source

---
+++ src/test/java/study/spring/securityauthentication/SecurityAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.securityauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
