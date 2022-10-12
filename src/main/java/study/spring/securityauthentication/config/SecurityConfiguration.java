6fb2985f795568bd39912a315e4ea8f00626b8b1
hclee <hclee@dataeum.kr>
2022-10-12 13:06:44 +0900

    CustomerLoginFilter 추가

--- src/main/java/study/spring/securityauthentication/config/SecurityConfiguration.java
+++ src/main/java/study/spring/securityauthentication/config/SecurityConfiguration.java
@@ -4,24 +4,50 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import study.spring.securityauthentication.core.domain.student.StudentManager;
+import study.spring.securityauthentication.core.domain.teacher.TeacherManager;
 
 @Configuration
 @EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
     private final StudentManager studentManager;
+    private final TeacherManager teacherManager;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        CustomerLoginFilter filter = new CustomerLoginFilter(authenticationManager(null));
         return httpSecurity
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                .antMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/", false)
+                .failureUrl("/login-error")
+                .and()
+                .addFilterAt(filter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied")
                 .and()
                 .build();
     }
@@ -34,5 +60,4 @@ public class SecurityConfiguration {
                 )
         );
     }
-
 }
